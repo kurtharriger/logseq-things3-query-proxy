25a5a60ee60ec3291a9c195611a1dc1019bf4da1
Kurt Harriger <kurtharriger@gmail.com>
2022-01-22 09:45:41 -0700

    cljstyle

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -5,55 +5,62 @@
             [cljs.core.async  :as async :refer [put! go-loop <!]]))
 
 
-(defn create-browser! []
+(defn create-browser!
+  []
   (let [ch (async/chan)
         window
         (BrowserWindow.
-         (clj->js {:width 800
-                   :height 600
-                   :show false
-                   :webPreferences 
-                   {;:nodeIntegration true
-                    ;todo: also need contextBridge to access ipcRenderer
-                    ; if this is enabled
-                    :contextIsolation false
-                    :preload (path/join js/__dirname "/preload.js")}}))]
+          (clj->js {:width 800
+                    :height 600
+                    :show false
+                    :webPreferences
+                    {;; :nodeIntegration true
+                     ;; todo: also need contextBridge to access ipcRenderer
+                     ;; if this is enabled
+                     :contextIsolation false
+                     :preload (path/join js/__dirname "/preload.js")}}))]
     (j/call window :loadURL (str "file://" js/__dirname "/public/index.html"))
     (j/call window :on "close"
-            (fn [e]
-              (j/call e :preventDefault true)
-              (j/call window :hide)
-              ;(put! ch [:quit])
-              ))
+      (fn [e]
+        (j/call e :preventDefault true)
+        (j/call window :hide)
+        ;; (put! ch [:quit])
+        ))
     {:window window
      :ch ch}))
 
 
-
-(defn create-tray! [on-click]
+(defn create-tray!
+  [on-click]
   ;; icon from https://www.flaticon.com/uicons?word=task
   (let [tray (Tray. "resources/public/img/tray.png")]
     (j/call tray :on "click" on-click)
     tray))
 
-(defn show-browser [window]
+
+(defn show-browser
+  [window]
   (j/call window :show))
 
-(defn start-ipc-listener! []
-  (j/call ipcMain :on "message" 
-          (fn [_ msg] (println "received msg" msg))))
+
+(defn start-ipc-listener!
+  []
+  (j/call ipcMain :on "message"
+    (fn [_ msg] (println "received msg" msg))))
 
 
 (def main-window (atom nil))
 (def system-tray (atom nil))
 
-(defn ready! []
+
+(defn ready!
+  []
   (start-ipc-listener!)
   (let [{:keys [window ch]} (create-browser!)
         tray (create-tray! (partial show-browser window))]
-    ; tray seems to disappear after about a minute when garbage collection
-    ; runs if not saved to global not sure why reference in go loop does not
-    ; prevent garbage collection
+    ;; tray seems to disappear after about a minute when garbage collection
+    ;; runs if not saved to global not sure why reference in go loop does not
+    ;; prevent garbage collection
     (reset! main-window window)
     (reset! system-tray tray)
     (go-loop []
@@ -64,8 +71,10 @@
           (j/call app :quit)))
       (recur))))
 
-(defn main []
-  ; CrashReporter can just be omitted
+
+(defn main
+  []
+  ;; CrashReporter can just be omitted
   ;; (.start crashReporter
   ;;         (clj->js
   ;;          {:companyName "MyAwesomeCompany"
@@ -75,5 +84,4 @@
 
   ;; (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
   ;;                                 (.quit app)))
-  (j/call app :on "ready" ready!)
-  )
+  (j/call app :on "ready" ready!))
--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -1,26 +1,33 @@
 (ns app.renderer.core
   (:require
-   ; note ipcRender must be added via preload.
-   ; standard require does not work
-  ; ["electron" :refer [ipcRenderer]]
-   [reagent.core :refer [atom]]
-   [reagent.dom :as rd]
-   [applied-science.js-interop :as j]
-   [cljs.core.async  :as async :refer [put! go-loop <!]]
-   ["peerjs" :as peer]))
+    ;; note ipcRender must be added via preload.
+    ;; standard require does not work
+    ;; ["electron" :refer [ipcRenderer]]
+    [reagent.core :refer [atom]]
+    [reagent.dom :as rd]
+    [applied-science.js-interop :as j]
+    [cljs.core.async  :as async :refer [put! go-loop <!]]
+    ["peerjs" :as peer]))
+
 
 (enable-console-print!)
 
 (defonce state (atom 0))
 
-(defn send-message! []
-  (j/call js/ipcRenderer :send "message" "test" ))
 
-(defn on-click []
+(defn send-message!
+  []
+  (j/call js/ipcRenderer :send "message" "test"))
+
+
+(defn on-click
+  []
   (send-message!)
   (swap! state inc))
 
-(defn root-component []
+
+(defn root-component
+  []
   [:div
    [:div.logos
     [:img.electron {:src "img/electron-logo.png"}]
@@ -30,7 +37,9 @@
     {:on-click on-click}
     (str "Clicked " @state " times")]])
 
-(defn ^:dev/after-load start! []
+
+(defn ^:dev/after-load start!
+  []
   (rd/render
-   [root-component]
-   (js/document.getElementById "app-container")))
+    [root-component]
+    (js/document.getElementById "app-container")))
