18a01b8aee7831e7cffe223a88d6ae6da7b9aebc
Kurt Harriger <kurtharriger@gmail.com>
2022-01-23 21:45:35 -0700

    experiment with change stream

--- src/app/main/db.cljs
+++ src/app/main/db.cljs
@@ -1,17 +1,17 @@
 (ns app.main.db
   (:require
-    ["fs" :as fs]
-    ["os" :as os]
-    ["sqlite3" :as sql]
-    [applied-science.js-interop :as j]
-    [clojure.string :as string]
-    [clojure.pprint :refer [pprint]]
-    [cljs.core.async :as async]
-    [com.rpl.specter :as s :refer-macros [select transform]]
-    [com.wsscode.async.async-cljs :as wa :refer [go-promise  <?maybe]]
-    [datascript.core :as d]
-    [medley.core :as medley]
-    [taoensso.timbre :refer [info error trace debug]]))
+   ["fs" :as fs]
+   ["os" :as os]
+   ["sqlite3" :as sql]
+   [applied-science.js-interop :as j]
+   [clojure.string :as string]
+   [clojure.pprint :refer [pprint]]
+   [cljs.core.async :as async]
+   [com.rpl.specter :as s :refer-macros [select transform]]
+   [com.wsscode.async.async-cljs :as wa :refer [go-promise  <?maybe]]
+   [datascript.core :as d]
+   [medley.core :as medley]
+   [taoensso.timbre :refer [info error trace debug]]))
 
 
 (defn coerce-date
@@ -40,19 +40,19 @@
 (defn query
   [row-transform sql db]
   (go-promise
-    (let [data (<?maybe
-                 (new js/Promise
-                   (fn [resolve]
-                     (j/call db :all sql (fn [err data]
-                                           (if err (resolve err) (resolve data)))))))
-          data (js->clj data :keywordize-keys true)
+   (let [data (<?maybe
+               (new js/Promise
+                    (fn [resolve]
+                      (j/call db :all sql (fn [err data]
+                                            (if err (resolve err) (resolve data)))))))
+         data (js->clj data :keywordize-keys true)
           ;; remove keys with nil values
-          data (remove-nil-values data)
-          data (mapv row-transform data)
+         data (remove-nil-values data)
+         data (mapv row-transform data)
           ;; again remove nils as some transforms may have removed values
-          data (remove-nil-values data)]
+         data (remove-nil-values data)]
 
-      data)))
+     data)))
 
 
 (defn setkeyns
@@ -70,8 +70,8 @@
 (defn area-transform
   [area]
   (-> area
-    (dissoc  :cachedTags)
-    (setkeyns "things.area")))
+      (dissoc  :cachedTags)
+      (setkeyns "things.area")))
 
 
 (def get-areas (partial query area-transform "select * from TMArea order by 'index'"))
@@ -80,27 +80,27 @@
 (defn task-transform
   [ns row]
   (-> row
-    (dissoc :cachedTags)
-    (dissoc :type)
-    (update :recurrenceRule str)
-    (update :repeater str)
-    (update :creationDate coerce-date)
-    (update :userModificationDate coerce-date)
-    (update :dueDate coerce-date)
-    (update :startDate coerce-date)
-    (update :stopDate coerce-date)
-    (update :instanceCreationStartDate coerce-date)
-    (update :afterCompletionReferenceDate coerce-date)
-    (update :lastAlarmInteractionDate coerce-date)
-    (update :todayIndexReferenceDate coerce-date)
-    (update :nextInstanceStartDate coerce-date)
-    (update :dueDateSuppressionDate coerce-date)
-    (update :repeaterMigrationDate coerce-date)
-    (update :area (as-ref :things.area/uuid))
-    (update :project (as-ref :things.project/uuid))
-    (update :actiongroup (as-ref :things.actiongroup/uuid))
-    (update :repeatingTemplate (as-ref :things.task/uuid))
-    (setkeyns ns)))
+      (dissoc :cachedTags)
+      (dissoc :type)
+      (update :recurrenceRule str)
+      (update :repeater str)
+      (update :creationDate coerce-date)
+      (update :userModificationDate coerce-date)
+      (update :dueDate coerce-date)
+      (update :startDate coerce-date)
+      (update :stopDate coerce-date)
+      (update :instanceCreationStartDate coerce-date)
+      (update :afterCompletionReferenceDate coerce-date)
+      (update :lastAlarmInteractionDate coerce-date)
+      (update :todayIndexReferenceDate coerce-date)
+      (update :nextInstanceStartDate coerce-date)
+      (update :dueDateSuppressionDate coerce-date)
+      (update :repeaterMigrationDate coerce-date)
+      (update :area (as-ref :things.area/uuid))
+      (update :project (as-ref :things.project/uuid))
+      (update :actiongroup (as-ref :things.actiongroup/uuid))
+      (update :repeatingTemplate (as-ref :things.task/uuid))
+      (setkeyns ns)))
 
 
 (def get-projects (partial query (partial task-transform "things.project") "select * from TMTask where type = 1 order by 'index'"))
@@ -117,11 +117,11 @@
 (defn checklistitem-transform
   [row]
   (-> row
-    (update :creationDate coerce-date)
-    (update :userModificationDate coerce-date)
-    (update :stopDate coerce-date)
-    (update :task (as-ref :things.task/uuid))
-    (setkeyns "things.checklistitem")))
+      (update :creationDate coerce-date)
+      (update :userModificationDate coerce-date)
+      (update :stopDate coerce-date)
+      (update :task (as-ref :things.task/uuid))
+      (setkeyns "things.checklistitem")))
 
 
 (def get-checklistitems (partial query checklistitem-transform "select * from TMChecklistItem order by 'index'"))
@@ -135,43 +135,22 @@
 (defn create-datascript-conn
   []
   (d/create-conn
-    {:things.area/uuid {:db/unique :db.unique/identity}
+   {:things.area/uuid {:db/unique :db.unique/identity}
 
-     :things.project/uuid {:db/unique :db.unique/identity}
-     :things.project/area {:db/valueType :db.type/ref}
+    :things.project/uuid {:db/unique :db.unique/identity}
+    :things.project/area {:db/valueType :db.type/ref}
 
-     :things.actiongroup/uuid {:db/unique :db.unique/identity}
-     :things.actiongroup/area {:db/valueType :db.type/ref}
-     :things.actiongroup/project {:db/valueType :db.type/ref}
+    :things.actiongroup/uuid {:db/unique :db.unique/identity}
+    :things.actiongroup/area {:db/valueType :db.type/ref}
+    :things.actiongroup/project {:db/valueType :db.type/ref}
 
-     :things.task/uuid {:db/unique :db.unique/identity}
-     :things.task/area {:db/valueType :db.type/ref}
-     :things.task/project {:db/valueType :db.type/ref}
-     :things.task/repeatingTemplate {:db/valueType :db.type/ref}
+    :things.task/uuid {:db/unique :db.unique/identity}
+    :things.task/area {:db/valueType :db.type/ref}
+    :things.task/project {:db/valueType :db.type/ref}
+    :things.task/repeatingTemplate {:db/valueType :db.type/ref}
 
-     :things.checklistitem/uuid  {:db/unique :db.unique/identity}
-     :things.checklistitem/task {:db/valueType :db.type/ref}}))
-
-
-(defn load-datascript!
-  [conn db]
-  (go-promise
-    (let [areas (<?maybe (get-areas db))
-          projects (<?maybe (get-projects db))
-          actiongroups (<?maybe (get-action-groups db))
-          tasks (<?maybe (get-tasks db))
-          checklistitems (<?maybe (get-checklistitems db))]
-      (try
-        (d/transact! conn
-          (concat areas
-            projects
-            actiongroups
-            tasks
-            checklistitems))
-        (info "Added " (count (d/datoms @conn :eavt)) "datums.")
-        (catch :default e
-          (do (error e)
-              (throw (ex-info "Error populating datascript db" {} e))))))))
+    :things.checklistitem/uuid  {:db/unique :db.unique/identity}
+    :things.checklistitem/task {:db/valueType :db.type/ref}}))
 
 
 (defn open-db
@@ -182,34 +161,85 @@
     (if exists (sql/Database. db-path)
         (throw (ex-info "database not found" {:path db-path})))))
 
+; sql-db
+; sql-db-path
+; sql-db-exists
+; datascript-conn
 
-(def default-db-path "~/Library/Group Containers/JLMPQHK86H.com.culturedcode.ThingsMac/Things Database.thingsdatabase/main.sqlite")
+(defn things-db [path]
+  (let [homedir (j/call os :homedir)
+        path (clojure.string/replace path #"^~" homedir)
+        exists? #(j/call fs :existsSync path)]
+    {:sql-db-exists? exists?
+     :sql-db-path path
+     :sql-db (delay (if (exists?)
+                      (sql/Database. path)
+                      (throw (ex-info "database not found" {:path path}))))
+     :datascript-conn (create-datascript-conn)
+     :listener-chans (atom {})}))
 
+(defn things-db-exists? [things-db] ((:sql-db-exists? things-db (constantly false))))
+(defn sql-db [things-db] @(:sql-db things-db))
+
+(defn listen! [things-db key]
+  (assert (nil? (-> things-db :listener-chans deref key)) "listener already registered")
+  (let [ch (async/chan)]
+    (swap! (:listener-chans things-db) assoc key ch)
+    (d/listen! (:datascript-conn things-db) key
+               (fn [{:keys [tx-data tempids]}]
+                 (when-not (empty? tx-data)
+                   (async/put! ch {:tx-id (:db/current-tx tempids)
+                                   :tx-data tx-data}))))
+    ch))
+
+(defn unlisten! [things-db key]
+  (d/unlisten! (:datascript-conn things-db) key)
+  (when-let [ch (-> things-db :listener-chans deref key)]
+    (async/close! ch))
+  (swap! (:listener-chans things-db) dissoc key))
+  
+(defn load-datascript!
+  [things-db]
+  (let [conn (:datascript-conn things-db)
+        db (sql-db things-db)]
+    
+    (go-promise
+     (let [areas (<?maybe (get-areas db))
+           projects (<?maybe (get-projects db))
+           actiongroups (<?maybe (get-action-groups db))
+           tasks (<?maybe (get-tasks db))
+           checklistitems (<?maybe (get-checklistitems db))]
+       (try
+         (d/transact! conn
+                      (concat areas
+                              projects
+                              actiongroups
+                              tasks
+                              checklistitems))
+         (catch :default e
+           (do (error e)
+               (throw (ex-info "Error populating datascript db" {} e)))))))))
+
+
+
+(def default-db (things-db "~/Library/Group Containers/JLMPQHK86H.com.culturedcode.ThingsMac/Things Database.thingsdatabase/main.sqlite"))
 
 ;; copied Things.sqlite3 from https://github.com/AlexanderWillner/things.sh 7104771af191eec196e4dff087ece02618b05e4c
-(def demo-db-path "resources/Things.sqlite3")
-
-
-(defonce default-db (delay (open-db default-db-path)))
-(defonce datascript-conn (create-datascript-conn))
-
-
-(defn begin-polling-changes!
-  ([]
-   (begin-polling-changes! datascript-conn @default-db))
-  ([conn db]
-   (async/go-loop []
-     (try (<?maybe (load-datascript! conn db)) (catch :default _ nil))
-     (<?maybe (async/timeout 1000))
-     (recur))))
-
+(def demo-db (things-db "resources/Things.sqlite3"))
 
 (comment
-  (defonce demodb (open-db demo-db-path))
-  (defonce db (open-db default-db-path))
+  ;; (defonce demodb (open-db demo-db-path))
+  ;; (defonce db (open-db default-db-path))
 
-  (count (:eavt @datascript-conn))
+  ;; (count (:eavt @datascript-conn))
 ;; 
+  (let [ch (listen! default-db :repl)]
+    (async/go-loop []
+      (when-let [changes (async/<! ch)]
+        (pprint changes) (recur))
+      (pprint "done.")))
+  
+
   (def last-gp (volatile! nil))
 
   (defn gp [value & args]
